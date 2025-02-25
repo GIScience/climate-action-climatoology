f8c4006a48c92bf3343013fe2e1521d3ba7d6431
Danielle Gatland <danielle.gatland@heigit.org>
2025-02-25 08:16:54 +0000

    fix(artifact): include index in geojson artifact

--- test/base/test_artifact.py
+++ test/base/test_artifact.py
@@ -233,9 +233,9 @@ def test_create_chart_artifact(
 EXPECTED_GEOJSON = """{
 "type": "FeatureCollection",
 "features": [
-{ "type": "Feature", "properties": { "color": "#fff", "label": "White a" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 1.0 ] } },
-{ "type": "Feature", "properties": { "color": "#000", "label": "Black b" }, "geometry": { "type": "Point", "coordinates": [ 2.0, 2.0 ] } },
-{ "type": "Feature", "properties": { "color": "#0f0", "label": "Green c" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
+{ "type": "Feature", "properties": { "index": 0, "color": "#fff", "label": "White a" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 1.0 ] } },
+{ "type": "Feature", "properties": { "index": 1, "color": "#000", "label": "Black b" }, "geometry": { "type": "Point", "coordinates": [ 2.0, 2.0 ] } },
+{ "type": "Feature", "properties": { "index": 2, "color": "#0f0", "label": "Green c" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
 ]
 }
 """
@@ -273,51 +273,6 @@ def test_create_geojson_artifact(default_computation_resources, general_uuid, de
         resources=default_computation_resources,
         filename=str(general_uuid),
         tags=default_association_tags,
-    )
-
-    assert generated_artifact == expected_artifact
-
-    with open(generated_artifact.file_path, 'r') as test_file:
-        generated_content = test_file.read()
-
-        assert generated_content == EXPECTED_GEOJSON
-
-
-def test_create_geojson_artifact_multiindex(default_computation_resources, general_uuid):
-    expected_artifact = _Artifact(
-        name='Test Vector',
-        modality=ArtifactModality.MAP_LAYER_GEOJSON,
-        file_path=Path(default_computation_resources.computation_dir / f'{general_uuid}.geojson'),
-        summary='Vector caption',
-        attachments={
-            AttachmentType.LEGEND: Legend(
-                legend_data={'Black b': Color('#000'), 'Green c': Color('#0f0'), 'White a': Color('#fff')}
-            )
-        },
-    )
-
-    index = pd.MultiIndex.from_tuples(
-        [('bar', 'one'), ('bar', 'two'), ('baz', 'one')],
-        names=['first', 'second'],
-    )
-    method_input = GeoDataFrame(
-        data={
-            'color': [Color((255, 255, 255)), Color((0, 0, 0)), Color((0, 255, 0))],
-            'label': ['White a', 'Black b', 'Green c'],
-            'geometry': [Point(1, 1), Point(2, 2), Point(3, 3)],
-        },
-        crs='EPSG:4326',
-        index=index,
-    )
-
-    generated_artifact = create_geojson_artifact(
-        features=method_input.geometry,
-        layer_name='Test Vector',
-        caption='Vector caption',
-        color=method_input.color.tolist(),
-        label=method_input.label.tolist(),
-        resources=default_computation_resources,
-        filename=str(general_uuid),
     )
 
     assert generated_artifact == expected_artifact
@@ -367,18 +322,174 @@ def test_create_geojson_artifact_continuous_legend(default_computation_resources
 
     assert generated_artifact == expected_artifact
 
+
+def test_create_geojson_artifact_index_str(default_computation_resources, general_uuid, default_association_tags):
+    expected_geojson = """{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "index": "hello", "color": "#fff", "label": "White a" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 1.0 ] } },
+{ "type": "Feature", "properties": { "index": "again", "color": "#000", "label": "Black b" }, "geometry": { "type": "Point", "coordinates": [ 2.0, 2.0 ] } },
+{ "type": "Feature", "properties": { "index": "world", "color": "#0f0", "label": "Green c" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
+]
+}
+"""
+
+    method_input = GeoDataFrame(
+        data={
+            'color': [Color((255, 255, 255)), Color((0, 0, 0)), Color((0, 255, 0))],
+            'label': ['White a', 'Black b', 'Green c'],
+            'geometry': [Point(1, 1), Point(2, 2), Point(3, 3)],
+        },
+        index=['hello', 'again', 'world'],
+        crs='EPSG:4326',
+    )
+
+    generated_artifact = create_geojson_artifact(
+        features=method_input.geometry,
+        color=method_input.color.tolist(),
+        label=method_input.label.tolist(),
+        layer_name='Test Vector',
+        caption='Vector caption',
+        resources=default_computation_resources,
+        filename=str(general_uuid),
+        tags=default_association_tags,
+    )
+
     with open(generated_artifact.file_path, 'r') as test_file:
         generated_content = test_file.read()
 
-        assert generated_content == EXPECTED_GEOJSON
+        assert generated_content == expected_geojson
+
+
+def test_create_geojson_artifact_index_non_unique(
+    default_computation_resources, general_uuid, default_association_tags
+):
+    expected_geojson = """{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "index": "hello", "color": "#fff", "label": "White a" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 1.0 ] } },
+{ "type": "Feature", "properties": { "index": "hello", "color": "#000", "label": "Black b" }, "geometry": { "type": "Point", "coordinates": [ 2.0, 2.0 ] } },
+{ "type": "Feature", "properties": { "index": "world", "color": "#0f0", "label": "Green c" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
+]
+}
+"""
+
+    method_input = GeoDataFrame(
+        data={
+            'color': [Color((255, 255, 255)), Color((0, 0, 0)), Color((0, 255, 0))],
+            'label': ['White a', 'Black b', 'Green c'],
+            'geometry': [Point(1, 1), Point(2, 2), Point(3, 3)],
+        },
+        index=['hello', 'hello', 'world'],
+        crs='EPSG:4326',
+    )
+
+    generated_artifact = create_geojson_artifact(
+        features=method_input.geometry,
+        color=method_input.color.tolist(),
+        label=method_input.label.tolist(),
+        layer_name='Test Vector',
+        caption='Vector caption',
+        resources=default_computation_resources,
+        filename=str(general_uuid),
+        tags=default_association_tags,
+    )
+
+    with open(generated_artifact.file_path, 'r') as test_file:
+        generated_content = test_file.read()
+
+        assert generated_content == expected_geojson
+
+
+EXPECTED_MULTIINDEX_GEOJSON = """{
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "index": "('bar', 'one')", "color": "#fff", "label": "White a" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 1.0 ] } },
+{ "type": "Feature", "properties": { "index": "('bar', 'two')", "color": "#000", "label": "Black b" }, "geometry": { "type": "Point", "coordinates": [ 2.0, 2.0 ] } },
+{ "type": "Feature", "properties": { "index": "('baz', 'one')", "color": "#0f0", "label": "Green c" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
+]
+}
+"""
+
+
+def test_create_geojson_artifact_multiindex(default_computation_resources, general_uuid):
+    expected_artifact = _Artifact(
+        name='Test Vector',
+        modality=ArtifactModality.MAP_LAYER_GEOJSON,
+        file_path=Path(default_computation_resources.computation_dir / f'{general_uuid}.geojson'),
+        summary='Vector caption',
+        attachments={
+            AttachmentType.LEGEND: Legend(
+                legend_data={'Black b': Color('#000'), 'Green c': Color('#0f0'), 'White a': Color('#fff')}
+            )
+        },
+    )
+
+    index = pd.MultiIndex.from_tuples(
+        [('bar', 'one'), ('bar', 'two'), ('baz', 'one')],
+        names=['first', 'second'],
+    )
+    method_input = GeoDataFrame(
+        data={
+            'color': [Color((255, 255, 255)), Color((0, 0, 0)), Color((0, 255, 0))],
+            'label': ['White a', 'Black b', 'Green c'],
+            'geometry': [Point(1, 1), Point(2, 2), Point(3, 3)],
+        },
+        crs='EPSG:4326',
+        index=index,
+    )
+
+    generated_artifact = create_geojson_artifact(
+        features=method_input.geometry,
+        layer_name='Test Vector',
+        caption='Vector caption',
+        color=method_input.color.tolist(),
+        label=method_input.label.tolist(),
+        resources=default_computation_resources,
+        filename=str(general_uuid),
+    )
+
+    assert generated_artifact == expected_artifact
+
+    with open(generated_artifact.file_path, 'r') as test_file:
+        generated_content = test_file.read()
+
+        assert generated_content == EXPECTED_MULTIINDEX_GEOJSON
+
+
+def test_create_geojson_artifact_tuple_index(default_computation_resources, general_uuid):
+    method_input = GeoDataFrame(
+        data={
+            'color': [Color((255, 255, 255)), Color((0, 0, 0)), Color((0, 255, 0))],
+            'label': ['White a', 'Black b', 'Green c'],
+            'geometry': [Point(1, 1), Point(2, 2), Point(3, 3)],
+        },
+        crs='EPSG:4326',
+        index=[('bar', 'one'), ('bar', 'two'), ('baz', 'one')],
+    )
+
+    generated_artifact = create_geojson_artifact(
+        features=method_input.geometry,
+        layer_name='Test Vector',
+        caption='Vector caption',
+        color=method_input.color.tolist(),
+        label=method_input.label.tolist(),
+        resources=default_computation_resources,
+        filename=str(general_uuid),
+    )
+
+    with open(generated_artifact.file_path, 'r') as test_file:
+        generated_content = test_file.read()
+
+        assert generated_content == EXPECTED_MULTIINDEX_GEOJSON
 
 
 EXPECTED_ROUNDING_GEOJSON = """{
 "type": "FeatureCollection",
 "features": [
-{ "type": "Feature", "properties": { "color": "#fff", "label": "Do Not Augment" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 0.9 ] } },
-{ "type": "Feature", "properties": { "color": "#000", "label": "Do Not Round" }, "geometry": { "type": "Point", "coordinates": [ 2.0000001, 1.9999999 ] } },
-{ "type": "Feature", "properties": { "color": "#0f0", "label": "Round Me" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
+{ "type": "Feature", "properties": { "index": 0, "color": "#fff", "label": "Do Not Augment" }, "geometry": { "type": "Point", "coordinates": [ 1.0, 0.9 ] } },
+{ "type": "Feature", "properties": { "index": 1, "color": "#000", "label": "Do Not Round" }, "geometry": { "type": "Point", "coordinates": [ 2.0000001, 1.9999999 ] } },
+{ "type": "Feature", "properties": { "index": 2, "color": "#0f0", "label": "Round Me" }, "geometry": { "type": "Point", "coordinates": [ 3.0, 3.0 ] } }
 ]
 }
 """
