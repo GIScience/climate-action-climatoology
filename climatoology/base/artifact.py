d919e088e858758bbcfa8274b0aa9b00e9121400
Moritz Schott <moritz.schott@heigit.org>
2025-12-12 10:12:30 +0000

    fix(artifact): assert each artifact has a unique filename
    closes #256

--- climatoology/base/artifact.py
+++ climatoology/base/artifact.py
@@ -173,7 +173,7 @@ class ArtifactMetadata(BaseModel):
     filename: str = Field(
         description='The name of the file that stores the artifact. Omit the extension.',
         examples=['my_first_artifact'],
-        default=uuid.uuid4(),
+        default_factory=lambda: uuid.uuid4(),
     )
     summary: str = Field(
         description='A short description of the artifact that could be used in a caption.',
--- test/base/artifact/test_artifact.py
+++ test/base/artifact/test_artifact.py
@@ -4,6 +4,7 @@ from pydantic import ValidationError
 from climatoology.base.artifact import (
     Artifact,
     ArtifactEnriched,
+    ArtifactMetadata,
     ArtifactModality,
 )
 
@@ -22,3 +23,10 @@ def test_artifact_filename_ascii_compliance_check():
             filename='test_artifact_file_$p€ciöl.md',
             summary='Test summary',
         )
+
+
+def test_artifact_filename_is_unique():
+    metadata_a = ArtifactMetadata(name='name', summary='summary')
+    metadata_b = ArtifactMetadata(name='name', summary='summary')
+
+    assert metadata_a.filename != metadata_b.filename
