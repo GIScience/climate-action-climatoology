6bd7f1db01077cbb575adc7d8eddaeddb101dcf6
Danielle Gatland <danielle.gatland@heigit.org>
2026-01-21 23:40:21 +0000

    fix(api): make sure generated bounds don't trim anything off the original bounds
    and round bounds before sending requests
    
    closes #263

--- test/utility/test_api.py
+++ test/utility/test_api.py
@@ -3,13 +3,20 @@ from geopandas import GeoSeries
 from sentinelhub import CRS, BBox, bbox_to_dimensions
 from shapely import MultiPolygon, Polygon, difference, geometry, unary_union
 
-from climatoology.utility.api import generate_bounds
+from climatoology.utility.api import _make_bound_dimensions_valid, generate_bounds
+
+
+def test_make_bound_dimensions_valid():
+    input_bounds = BBox((8.6766405, 49.4149631, 8.6920303, 49.4149632), crs=CRS.WGS84)
+
+    computed_bounds = _make_bound_dimensions_valid(bounds=input_bounds, resolution=10)
+    assert computed_bounds.geometry.covers(input_bounds.geometry)
 
 
 @pytest.mark.parametrize(['max_unit_size', 'expected_n_bounds'], [[500, 9], [800, 4], [1500, 1]])
 def test_generate_bounds_max_edge_length(max_unit_size, expected_n_bounds):
     # Actual h, w = (1336, 1304)
-    origin_area = GeoSeries([geometry.box(8.0859375, 47.5172006978394, 8.26171875, 47.63578359086485)])
+    origin_area = GeoSeries([geometry.box(8.0859375, 47.5172007, 8.2617188, 47.6357836)])
 
     bounds = generate_bounds(target_geometries=origin_area, resolution=10, max_unit_size=max_unit_size)
     adjusted_area = unary_union([geometry.box(*b) for b in bounds])
@@ -21,7 +28,7 @@ def test_generate_bounds_max_edge_length(max_unit_size, expected_n_bounds):
 @pytest.mark.parametrize(['max_unit_area', 'expected_n_bounds'], [[400 * 400, 16], [800 * 800, 4], [1400 * 1400, 1]])
 def test_generate_bounds_max_area(max_unit_area, expected_n_bounds):
     # Actual h, w = (1336, 1304)
-    origin_area = GeoSeries([geometry.box(8.0859375, 47.5172006978394, 8.26171875, 47.63578359086485)])
+    origin_area = GeoSeries([geometry.box(8.0859375, 47.5172007, 8.2617188, 47.6357836)])
 
     bounds = generate_bounds(target_geometries=origin_area, resolution=10, max_unit_area=max_unit_area)
     adjusted_area = unary_union([geometry.box(*b) for b in bounds])
@@ -55,21 +62,45 @@ def test_generate_bounds_with_invalid_dimension():
     assert actual_splits == expected_bounds
 
 
+def test_generate_bounds_not_clipped():
+    # The top point of this feature was clipped by a previous method of generating bounds, which transformed and
+    # extended the bounds based on only the bottom left and top right corners of the bounding box
+    origin_area = GeoSeries(
+        [
+            Polygon(
+                [
+                    [-46.7119327, -23.7710972],
+                    [-46.7120312, -23.7710941],
+                    [-46.7118835, -23.7693584],
+                ]
+            )
+        ]
+    )
+
+    computed_bounds = generate_bounds(target_geometries=origin_area, resolution=120, max_unit_size=1000)
+    actual_splits = [bbox_to_dimensions(b, resolution=10) for b in computed_bounds]
+
+    expected_bounds = [(12, 20)]
+    assert actual_splits == expected_bounds
+
+    assert origin_area.iloc[0].within(computed_bounds[0].geometry)
+
+
 def test_generate_bounds_from_multipolygon_drops_unused_splits():
     """If the input is a multipolygon which only overlaps some of the split bounds, the 'unused' bounds should be
     dropped from the computed bounds.
     """
     expected_bounds = [
-        BBox(((8.0859375, 47.5172006978394), (8.311197916666666, 47.676492144352125)), crs=CRS.WGS84),
-        BBox(((8.536458333333334, 47.676492144352125), (8.76171875, 47.83578359086485)), crs=CRS.WGS84),
+        BBox(((8.0859375, 47.5172007), (8.3111979, 47.6764922)), crs=CRS.WGS84),
+        BBox(((8.5364584, 47.6764922), (8.7617188, 47.8357836)), crs=CRS.WGS84),
     ]
 
     multipolygon = GeoSeries(
         [
             MultiPolygon(
                 [
-                    geometry.box(8.0859375, 47.5172006978394, 8.26171875, 47.63578359086485),
-                    geometry.box(8.5859375, 47.7172006978394, 8.76171875, 47.83578359086485),
+                    geometry.box(8.0859375, 47.5172007, 8.2617188, 47.6357836),
+                    geometry.box(8.5859375, 47.7172007, 8.7617188, 47.8357836),
                 ]
             )
         ]
